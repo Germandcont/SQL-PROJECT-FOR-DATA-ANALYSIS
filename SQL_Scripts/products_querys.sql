eccf26b5eeb1736846416eba71eba795b57db132
Germán Domínguez <gdominguezcon@gmail.com>
2024-10-21 16:04:45 +0200

    Update products_querys.sql

--- SQL_Scripts/products_querys.sql
+++ SQL_Scripts/products_querys.sql
@@ -1,7 +1,15 @@
 /* Total Products */
 
+SELECT COUNT(DISTINCT ProductID) AS NumberOfProducts
+FROM SalesLT.Product
+WHERE DiscontinuedDate IS NULL;
+
+
+/* Total Of Different Products Sold */
+
 SELECT COUNT(DISTINCT p.ProductID) AS NumberOfProducts
 FROM SalesLT.Product p
 JOIN SalesLT.SalesOrderDetail sod ON p.ProductID = sod.ProductID
 JOIN SalesLT.SalesOrderHeader soh ON sod.SalesOrderID = soh.SalesOrderID;
 
+/* FROM 295 PRODUCTS, THE COMPANY HAS ONLY SOLD 142 IN THIS PERIOD OF TIME */
