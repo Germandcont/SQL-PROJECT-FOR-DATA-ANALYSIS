9e11ebd71465d7eb389cd0fce3aee513878f8fa1
Germán Domínguez <gdominguezcon@gmail.com>
2024-10-21 16:29:02 +0200

    Update products_querys.sql

--- SQL_Scripts/products_querys.sql
+++ SQL_Scripts/products_querys.sql
@@ -13,3 +13,13 @@ JOIN SalesLT.SalesOrderDetail sod ON p.ProductID = sod.ProductID
 JOIN SalesLT.SalesOrderHeader soh ON sod.SalesOrderID = soh.SalesOrderID;
 
 /* FROM 295 PRODUCTS, THE COMPANY HAS ONLY SOLD 142 IN THIS PERIOD OF TIME */
+
+
+/* Top 5 products by sales */
+
+SELECT TOP 5 p.Name AS ProductName, SUM(od.OrderQty) AS TotalSold
+FROM SalesLT.Product AS p
+JOIN SalesLT.SalesOrderDetail AS od ON p.ProductID = od.ProductID
+GROUP BY p.Name
+ORDER BY TotalSold DESC;
+
